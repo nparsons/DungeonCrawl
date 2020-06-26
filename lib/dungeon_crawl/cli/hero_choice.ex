601fb31247c136769ca07471bcfe1e975799c1a3
Nicholas Parsons <parsons.nicholas1@gmail.com>
2020-06-26 14:35:19 -0400

    Added hero choice functionality to game.

---
+++ lib/dungeon_crawl/cli/hero_choice.ex
@@ -0,0 +1,50 @@
+defmodule DungeonCrawl.CLI.HeroChoice do
+  alias Mix.Shell.IO, as: Shell
+
+  def start do
+    Shell.cmd("clear")
+    Shell.info("Start by choosing your hero:")
+
+    heroes = DungeonCrawl.Heroes.all()
+    find_hero_by_index = &Enum.at(heroes, &1)
+
+    heroes
+    |> Enum.map(& &1.name)
+    |> display_options
+    |> generate_question
+    |> Shell.prompt()
+    |> parse_answer
+    |> find_hero_by_index.()
+    |> confirm_hero
+  end
+
+  defp display_options(options) do
+    options
+    |> Enum.with_index(1)
+    |> Enum.each(fn {option, index} ->
+      Shell.info("#{index} - #{option}")
+    end)
+
+    options
+  end
+
+  defp generate_question(options) do
+    options = Enum.join(1..Enum.count(options), ",")
+    "Which one? [#{options}]\n"
+  end
+
+  defp parse_answer(answer) do
+    {option, _} = Integer.parse(answer)
+    option - 1
+  end
+
+  defp confirm_hero(chosen_hero) do
+    Shell.cmd("clear")
+    Shell.info(chosen_hero.description)
+
+    cond do
+      Shell.yes?("Confirm?") -> chosen_hero
+      true -> start()
+    end
+  end
+end
