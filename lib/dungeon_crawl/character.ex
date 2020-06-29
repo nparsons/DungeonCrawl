050497a4cd6262eb00271e21da875bb7497e6c37
Nicholas Parsons <parsons.nicholas1@gmail.com>
2020-06-29 13:53:05 -0400

    Added enemies and battles to the game.

--- lib/dungeon_crawl/character.ex
+++ lib/dungeon_crawl/character.ex
@@ -1,12 +1,12 @@
 defmodule DungeonCrawl.Character do
-@type t :: %DungeonCrawl.Character{
-        name: String.t,
-        description: String.t,
-        hit_points: non_neg_integer,
-        max_hit_points: non_neg_integer,
-        attack_description: String.t,
-        damage_range: Range.t
-      }
+  @type t :: %DungeonCrawl.Character{
+          name: String.t(),
+          description: String.t(),
+          hit_points: non_neg_integer,
+          max_hit_points: non_neg_integer,
+          attack_description: String.t(),
+          damage_range: Range.t()
+        }
   defstruct name: nil,
             description: nil,
             hit_points: 0,
@@ -17,6 +17,23 @@ defmodule DungeonCrawl.Character do
   defimpl String.Chars do
     def to_string(character), do: character.name
   end
+
+  def take_damage(character, damage) do
+    new_hit_points = max(0, character.hit_points - damage)
+    %{character | hit_points: new_hit_points}
+  end
+
+  def heal(character, healing_value) do
+    new_hit_points =
+      min(
+        character.hit_points + healing_value,
+        character.max_hit_points
+      )
+
+    %{character | hit_points: new_hit_points}
+  end
+
+  def current_stats(character) do
+    "Player Stats\nHP: #{character.hit_points}/#{character.max_hit_points}"
+  end
 end
-
-
